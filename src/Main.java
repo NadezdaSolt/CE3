26a44433a7a0a4822453ea5639441be858308461
NadezdaSolt <nadiyaleonova1995@yandex.ru>
2019-05-04 21:36:59 +0300

    DONE pr3 https://www.codeabbey.com/index/task_view/sums-in-loop--ru

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int strNum = scanner.nextInt();
-        int [] arrayA = new int[strNum];
-        int [] arrayB = new int[strNum];
-        for (int i = 0; i < strNum; i++){
-            arrayA[i] = scanner.nextInt();}
-        for (int j = 0; j < strNum; j++){
-            arrayB[j] = scanner.nextInt();}
-        /*int[] r = new int[arrayA.length + arrayB.length];
-        System.arraycopy(arrayA, 0, r, 0, arrayA.length);
-        System.arraycopy(arrayB, 0, r, arrayB.length, arrayB.length);
-        String intArrayString = Arrays.toString(r);
-        System.out.println(intArrayString);*/
-        for (int i = 0; i < 3; i++)
+        scanner.nextLine();
+        int[][] array = new int[strNum][2];
+        int sum;
+        for (int i = 0; i < strNum; i++) {
             for (int j = 0; j < 2; j++) {
-                int [] sum = arrayA[i]+ arrayB[j];}
-    }
+                array[i][j] = scanner.nextInt();
+            }
+            sum = array[i][0] + array[i][1];
+            System.out.println(sum + " ");
+        }
     }
 }
